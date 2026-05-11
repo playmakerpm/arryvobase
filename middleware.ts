4a4c94086d0dc9c97d10cf1dfd7e65ae56a08804
Carlitos <Old_desktop@Carloss-MacBook-Pro.local>
2026-05-11 14:47:34 -0700

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -12,6 +13,7 @@ export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
     await auth.protect();
   }
+  return NextResponse.next();
 });
 
 export const config = {
